23d6323fccdb3bf8d728c8c280e6a4317b028b0a
DanielGarcia_laptop <dangargue2@alu.edu.gva.es>
2024-12-06 10:54:39 +0100

    public

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -7,7 +7,7 @@ export function HeaderComponent() {
                 <li>
                     <IconComponent
                         icono="fa-solid fa-file-pdf"
-                        link="dist/assets/DanielGarcia.pdf" />
+                        link="assets/DanielGarcia.pdf" />
                 </li>
                 <li>
                     <IconComponent
