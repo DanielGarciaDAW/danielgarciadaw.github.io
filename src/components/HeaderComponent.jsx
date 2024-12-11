6bb059d4dcefec55d32ca01569d5d2117e8428f1
Dani Daw <dangargue2@alu.edu.gva.es>
2024-12-11 17:13:02 +0100

    Version 2.2 Bug

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -10,26 +10,28 @@ export function HeaderComponent() {
             <div className="pf-header-logo">
                 <LogoComponent />
             </div>
-            <ul className="pf-header-nav-bar">
-                <li>
-                    <span className="pf-header-email pf-header-button slide_right"><ContactLink/></span>
-                </li>
-                <li>
-                    <IconComponent
-                        icono="fa-solid fa-file-pdf"
-                        link="assets/DanielGarcia.pdf" />
-                </li>
-                <li>
-                    <IconComponent
-                        icono="fa-brands fa-linkedin"
-                        link="https://lnkd.in/dAxxxUyS" />
-                </li>
-                <li>
-                    <IconComponent
-                        icono="fa-brands fa-github"
-                        link="https://github.com/DanielGarciaDaw" />
-                </li>
-            </ul>
+
+            <div className="pf-header-nav">
+                <span className="pf-header-email pf-header-button slide_right"><ContactLink /></span>
+
+                <ul className="pf-header-nav-bar">
+                    <li>
+                        <IconComponent
+                            icono="fa-solid fa-file-pdf"
+                            link="assets/DanielGarcia.pdf" />
+                    </li>
+                    <li>
+                        <IconComponent
+                            icono="fa-brands fa-linkedin"
+                            link="https://lnkd.in/dAxxxUyS" />
+                    </li>
+                    <li>
+                        <IconComponent
+                            icono="fa-brands fa-github"
+                            link="https://github.com/DanielGarciaDaw" />
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 }
