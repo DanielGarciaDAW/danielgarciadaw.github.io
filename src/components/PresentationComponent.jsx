3084e63306e3ee36a206e53142b79ee059d4cd4b
Dani Daw <dangargue2@alu.edu.gva.es>
2024-12-11 13:32:38 +0100

    preparando 2.0

--- src/components/PresentationComponent.jsx
+++ src/components/PresentationComponent.jsx
@@ -5,11 +5,11 @@ export function PresentationComponent() {
         <section className="pf-presentation-content" style={{ '--td': '1.2s' }}>
             <div className="pf-presentation">
                 <h1 className="pf-presentation-title block-reveal"
-                style={{ '--bc': '#623bbd', '--d': '.1s' }}>
+                style={{ '--bc': '#fc2f68', '--d': '.1s' }}>
                 Daniel García
             </h1>
             <p className="pf-presentation-description block-reveal"
-                style={{ '--bc': '#b23bbd', '--d': '.5s' }}>
+                style={{ '--bc': '#f9ebc4', '--d': '.5s' }}>
                 Full Stack Developer
             </p>
             </div>
