91493efffaba975c7b831d1147c22308dbe6178b
Dani Daw <dangargue2@alu.edu.gva.es>
2024-12-11 17:51:30 +0100

    cosetes

--- src/components/PresentationComponent.jsx
+++ src/components/PresentationComponent.jsx
@@ -5,13 +5,19 @@ export function PresentationComponent() {
         <section className="pf-presentation-content" style={{ '--td': '1.2s' }}>
             <div className="pf-presentation">
                 <h1 className="pf-presentation-title block-reveal"
-                style={{ '--bc': '#fc2f68', '--d': '.1s' }}>
-                Daniel García
-            </h1>
-            <p className="pf-presentation-description block-reveal"
-                style={{ '--bc': '#f9ebc4', '--d': '.5s' }}>
-                Full Stack Developer
-            </p>
+                    style={{ '--bc': '#fc2f68', '--d': '.1s' }}>
+                    Daniel García
+                </h1>
+                <p className="pf-presentation-dev block-reveal"
+                    style={{ '--bc': '#f9ebc4', '--d': '.5s' }}>
+                    Full Stack Developer
+                </p>
+            </div>
+            <div className="pf-presentation-description">
+                <h4>"Apasionado de la programación,
+                actualmente estudiante del Ciclo Superior de Desarrollo de Aplicaciones Web
+                Me encanta crear soluciones innovadoras y funcionales, y disfruto
+                aprendiendo nuevas tecnologías para mejorar mis habilidades."</h4>
             </div>
         </section>
     );
